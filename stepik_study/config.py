641608ca8903ce8d68c23f39b266123ad7f8132c
Daria <d.digalova@gmail.com>
2020-08-22 19:15:03 +0300

    upgrade database link

--- stepik_study/config.py
+++ stepik_study/config.py
@@ -1,4 +1,8 @@
+import os
+
+
 class Config:
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SECRET_KEY = 'my-super-secret-phrase-I-dont-tell-this-to-nobody'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
